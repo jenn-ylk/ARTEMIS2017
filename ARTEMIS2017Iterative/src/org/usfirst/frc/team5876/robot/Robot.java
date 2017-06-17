5e4dbce489692c503f6889f349f8e40f31f5d132
williamsk2019 <williamsk2019@ACEE-JQ2FM72.abbotsleigh.nsw.edu.au>
2017-06-17 11:07:54 +1000

    removed some bits of comment that were annoying me

--- ARTEMIS2017Iterative/src/org/usfirst/frc/team5876/robot/Robot.java
+++ ARTEMIS2017Iterative/src/org/usfirst/frc/team5876/robot/Robot.java
@@ -10,13 +10,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.Timer;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
- */
+
 public class Robot extends IterativeRobot {
 	final Integer middleAuto = new Integer(0);
 	final Integer rightAuto = new Integer(1);
@@ -36,21 +30,17 @@ public class Robot extends IterativeRobot {
 	ADXRS450_Gyro gyro;
 	Timer timer;
 
-	/**
-	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
-	 */
+	
 	 @Override
 	 public void robotInit() {
 		 chooser.addDefault("Middle Gear", middleAuto);
 		 chooser.addObject("Right Gear", rightAuto);
 		 chooser.addObject("Left Gear", leftAuto);
 		 SmartDashboard.putData("Auto choices", chooser);
-		 // chooser.addObject("Middle Gear", new Integer(0));
-		 // chooser.addObject("Right", new Integer(1));  
+		 
+		 
 		 stick = new Joystick(0);
 		 gamepad = new Joystick(1);
-
 		 SpeedController driveLeftFront = new VictorSP(0);
 		 SpeedController driveLeftBack = new VictorSP(1);
 		 SpeedController driveRightFront = new VictorSP(2);
@@ -68,31 +58,16 @@ public class Robot extends IterativeRobot {
 		 robotDrive = new RobotDrive(driveLeftFront, driveLeftBack, driveRightFront, driveRightBack);
 	 }
 
-	 /**
-	  * This autonomous (along with the chooser code above) shows how to select
-	  * between different autonomous modes using the dashboard. The sendable
-	  * chooser code works with the Java SmartDashboard. If you prefer the
-	  * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	  * getString line to get the auto name from the text box below the Gyro
-	  *
-	  * You can add additional auto modes by adding additional comparisons to the
-	  * switch structure below with additional strings. If using the
-	  * SendableChooser make sure to add them to the chooser code above as well.
-	  */
 	 @Override
 	 public void autonomousInit() {
 		 timer.reset();
 		 timer.start();
 		 gyro.reset();
 		 autoSelected = chooser.getSelected();
-		 // autoSelected = SmartDashboard.getString("Auto Selector",
-				 // defaultAuto);
 		 System.out.println("Auto selected: " + autoSelected);
 	 }
 
-	 /**
-	  * This function is called periodically during autonomous
-	  */
+	
 	 @Override
 	 public void autonomousPeriodic() {
 		 while (isAutonomous() && isEnabled()){
@@ -158,19 +133,11 @@ public class Robot extends IterativeRobot {
 		 }
 	 }
 
-	 /**
-	  * This function is called periodically during operator control
-	  */
+	
 	 @Override
 	 public void teleopPeriodic() {
 		 System.out.println(gyro.getAngle());
-		 /*
-		  * if (stick.getRawButton(0) == true) { gyro.reset(); while
-		  * (stick.getRawButton(0) == true) { driveForward(); }
-		  */
-		 // robotDrive.arcadeDrive((stick.getRawAxis(1)), -(stick.getRawAxis(4)),
-		 // true); //using the gamepad
-		 // }
+	
 		 int pov = stick.getPOV();
 		 boolean button = gamepad.getRawButton(1);
 		 float slow = 0.5f;
